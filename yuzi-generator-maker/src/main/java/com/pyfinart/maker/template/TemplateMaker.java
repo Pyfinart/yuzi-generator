ec7136ea1bee87d416633a14ba1517fb0ccfdf0d
pyfinart <mypyfinart@gmail.com>
2024-01-25 00:01:39 +0800

    针对MainTemplate.java，制作模板能力增强，之前的版本只是针对一个文件进行修改，现在的版本可以针对多个文件；或者多个目录下的所有层级的文件，进行制作模板，并生成一个meta.json文件。我们同样可以多次迭代制作

--- yuzi-generator-maker/src/main/java/com/pyfinart/maker/template/TemplateMaker.java
+++ yuzi-generator-maker/src/main/java/com/pyfinart/maker/template/TemplateMaker.java
@@ -13,6 +13,7 @@ import com.pyfinart.maker.meta.enums.FileTypeEnum;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -22,7 +23,18 @@ import java.util.stream.Collectors;
  */
 public class TemplateMaker {
 
-    private static long makeTemplate(Meta newMeta, Meta.ModelConfigDTO.ModelDTO modelDTO, String originalProjectPath, String fileInputPath, String searchStr, Long id) {
+    /**
+     * 制作模板和meta文件
+     *
+     * @param newMeta             meta文件对应的对象
+     * @param modelDTO            要挖坑的位置对应的模型
+     * @param originalProjectPath 原始文件的路径
+     * @param fileInputPathList   原始文件路径下的用来制作模板的文件的相对路径
+     * @param searchStr           用于搜索替换挖坑的内容
+     * @param id                  命名空间id，用于区别不同次的生成项目，以及多次对同一生成内容做进一步模板挖坑
+     * @return 命名空间id
+     */
+    private static long makeTemplate(Meta newMeta, Meta.ModelConfigDTO.ModelDTO modelDTO, String originalProjectPath, List<String> fileInputPathList, String searchStr, Long id) {
         // 没有 id 则生成
         if (id == null) {
             id = IdUtil.getSnowflakeNextId();
@@ -34,7 +46,7 @@ public class TemplateMaker {
 
         // 复制目录
         String tempDirPath = projectPath + File.separator + ".temp";
-        String templatePath = tempDirPath + File.separator + id;
+        String templatePath = tempDirPath + File.separator + id; // /.../yuzi-generator/yuzi-generator-maker/.temp/1749794565135020032
 
         // 目录不存在，则为首次制作
         if (!FileUtil.exist(templatePath)) {
@@ -45,41 +57,29 @@ public class TemplateMaker {
         // 一。输入信息
 
         // 2. 输入文件的信息
+        // /.../yuzi-generator/yuzi-generator-maker/.temp/1749794565135020032/acm-template
         String sourcePath = templatePath + File.separator + FileUtil.getLastPathEle(Paths.get(originalProjectPath)).toString();
 
-        String fileOutputPath = fileInputPath + ".ftl";
-
-
-        // 二、 使用字符串替换生成模板文件
-        // 1. 挖坑
-        String fileAbsoluteInputPath = sourcePath + File.separator + fileInputPath;
-        String fileAbsoluteOutputPath = sourcePath + File.separator + fileOutputPath;
-
-        // 若存在，则不是第一次生成，在已有基础上做修改
-        String fileContent;
-        if (FileUtil.exist(fileAbsoluteOutputPath)) {
-            fileContent = FileUtil.readUtf8String(fileAbsoluteOutputPath);
-        } else {
-            fileContent = FileUtil.readUtf8String(fileAbsoluteInputPath);
+        // 二、模板文件生成
+        ArrayList<Meta.FileConfigDTO.FileDTO> fileDTOS = new ArrayList<>();
+        for (String fileInputPath : fileInputPathList) {
+            String fileAbsoluteInputPath = sourcePath + File.separator + fileInputPath;
+            if (FileUtil.isDirectory(fileAbsoluteInputPath)) {
+                List<File> files1 = FileUtil.loopFiles(fileAbsoluteInputPath);
+                for (File file : files1) {
+                    Meta.FileConfigDTO.FileDTO fileDTO = makeFileTemplate(modelDTO, file, searchStr, sourcePath);
+                    if (fileDTO == null) continue;
+                    fileDTOS.add(fileDTO);
+                }
+            } else {
+                Meta.FileConfigDTO.FileDTO fileDTO = makeFileTemplate(modelDTO, new File(fileInputPath), searchStr, sourcePath);
+                fileDTOS.add(fileDTO);
+            }
         }
-
-        String replacement = String.format("${%s}", modelDTO.getFieldName());
-        String newFileContent = StrUtil.replace(fileContent, searchStr, replacement);
-
-        // 2. 写入模板文件
-        FileUtil.writeUtf8String(newFileContent, fileAbsoluteOutputPath);
-
-        // 三、生成元信息配置文件
-        // 实现思路，首先在程序中实例化Meta类，并填充参数，在写入文件
-
-        Meta.FileConfigDTO.FileDTO fileDTO = new Meta.FileConfigDTO.FileDTO();
-        fileDTO.setInputPath(fileInputPath);
-        fileDTO.setOutputPath(fileOutputPath);
-        fileDTO.setType(FileTypeEnum.FILE.getValue());
-        fileDTO.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
 
         String metaOutputPath = sourcePath + File.separator + "meta.json";
 
+        // 三、元信息文件生成
         // 若meta.json存在，说明现在是在追加或者修改元信息
         if (FileUtil.exist(metaOutputPath)) {
             Meta oldMeta = JSONUtil.toBean(FileUtil.readUtf8String(metaOutputPath), Meta.class);
@@ -88,7 +88,7 @@ public class TemplateMaker {
 
             // 1. 追加配置参数
             List<Meta.FileConfigDTO.FileDTO> files = newMeta.getFileConfig().getFiles();
-            files.add(fileDTO);
+            files.addAll(fileDTOS);
             List<Meta.ModelConfigDTO.ModelDTO> models = newMeta.getModelConfig().getModels();
             models.add(modelDTO);
 
@@ -107,7 +107,7 @@ public class TemplateMaker {
             List<Meta.FileConfigDTO.FileDTO> fileDTOList = new ArrayList<>();
             fileConfigDTO.setFiles(fileDTOList);
 
-            fileDTOList.add(fileDTO);
+            fileDTOList.addAll(fileDTOS);
 
             Meta.ModelConfigDTO modelConfig = new Meta.ModelConfigDTO();
             newMeta.setModelConfig(modelConfig);
@@ -122,6 +122,68 @@ public class TemplateMaker {
 
 
         return id;
+    }
+
+    /**
+     * 制作文件模板
+     *
+     * @param modelDTO      要挖坑的位置对应的模型
+     * @param fileInputFile 原始文件路径下的用来制作模板的文件的绝对路径File对象
+     * @param searchStr     用于搜索替换挖坑的内容
+     * @param sourcePath    存放制作好的模板代码项目的根路径：/.../yuzi-generator/yuzi-generator-maker/.temp/1749794565135020032/acm-template
+     * @return
+     */
+    private static Meta.FileConfigDTO.FileDTO makeFileTemplate(Meta.ModelConfigDTO.ModelDTO modelDTO, File fileInputFile, String searchStr, String sourcePath) {
+
+        // 挖坑文件相对路径
+        String fileInputPath = fileInputFile.getAbsolutePath().replace(sourcePath + "/", "");
+        if (StrUtil.endWith(fileInputPath, ".ftl")) return null;
+
+        String fileOutputPath = fileInputPath + ".ftl";
+
+
+        // 二、 使用字符串替换生成模板文件
+        // 1. 挖坑
+        String fileAbsoluteInputPath = fileInputFile.getAbsolutePath();
+        String fileAbsoluteOutputPath = fileInputFile.getAbsolutePath() + ".ftl";
+
+        // 若存在，则不是第一次生成，在已有基础上做修改
+        String fileContent;
+        if (FileUtil.exist(fileAbsoluteOutputPath)) {
+            fileContent = FileUtil.readUtf8String(fileAbsoluteOutputPath);
+        } else {
+            fileContent = FileUtil.readUtf8String(fileAbsoluteInputPath);
+        }
+
+        String replacement = String.format("${%s}", modelDTO.getFieldName());
+        String newFileContent = StrUtil.replace(fileContent, searchStr, replacement);
+
+
+        // 三、生成元信息配置文件
+        // 实现思路，首先在程序中实例化Meta类，并填充参数，在写入文件
+
+        Meta.FileConfigDTO.FileDTO fileDTO = new Meta.FileConfigDTO.FileDTO();
+        fileDTO.setInputPath(fileInputPath);
+        fileDTO.setOutputPath(fileOutputPath);
+        fileDTO.setType(FileTypeEnum.FILE.getValue());
+
+        // 写入模板文件
+        // 要先判断改文件是否真的有挖坑，没有说明是静态生成，不需要ftl模板
+        if (newFileContent.equals(fileContent)) {
+            if (FileUtil.exist(fileAbsoluteOutputPath)) {
+                fileDTO.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
+            } else {
+                // 输出路径 = 输入路径
+                fileDTO.setOutputPath(fileInputPath);
+                fileDTO.setGenerateType(FileGenerateTypeEnum.STATIC.getValue());
+            }
+
+        } else {
+            fileDTO.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
+            FileUtil.writeUtf8String(newFileContent, fileAbsoluteOutputPath);
+        }
+
+        return fileDTO;
     }
 
     /**
@@ -163,15 +225,19 @@ public class TemplateMaker {
         modelDTO.setType("String");
 
         String projectPath = System.getProperty("user.dir");
-        String originalProjectPath = new File(projectPath).getParent() + File.separator + "samples/acm-template";
-        String fileInputPath = "src/com/yupi/acm/MainTemplate.java"; // 因为方法中有了sourcePath，文件路径只需要相对路径
+        String originalProjectPath = new File(projectPath).getParent() + File.separator + "samples/springboot-init";
+        String fileInputPath = "src/main/java/com/yupi/springbootinit"; // 因为方法中有了sourcePath，文件路径只需要相对路径
 
 //        String searchStr = "Sum: ";
 
         // 替换变量（第二次）
-        String searchStr = "MainTemplate";
+        String searchStr = "BaseResponse";
 
-        long l = makeTemplate(meta, modelDTO, originalProjectPath, fileInputPath, searchStr, 1749794565135020032l);
+        String inputFilePath1 = "src/main/java/com/yupi/springbootinit/common";
+        String inputFilePath2 = "src/main/java/com/yupi/springbootinit/controller";
+        List<String> inputFilePathList = Arrays.asList(inputFilePath1, inputFilePath2);
+
+        long l = makeTemplate(meta, modelDTO, originalProjectPath, inputFilePathList, searchStr, null);
         System.out.println(l);
     }
 
